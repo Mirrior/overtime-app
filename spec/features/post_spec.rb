39a533246e64a22393530493f27ef491138ddbf1
Rustin Player <rjplayer@outlook.com>
2018-02-20 15:39:43 -0700

    Refactored index page to use Post partials

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do 
-    user = User.create!(email: "test@test.test", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-    login_as(user, :scope => :user)
+    @user = User.create!(email: "test@test.test", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+    login_as(@user, :scope => :user)
   end
 
   describe "index" do
@@ -19,8 +19,8 @@ describe 'navigate' do
     end
 
     it "has a list of posts" do
-      post1 = Post.create(date: Date.today, rationale: "Maybe in Russia", user_id: User.last.id)
-      post2 = Post.create(date: Date.today, rationale: "Probably in Russia", user_id: User.last.id)
+      post1 = Post.create(date: Date.today, rationale: "Maybe in Russia", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "Probably in Russia", user_id: @user.id)
       visit posts_path
       expect(page).to have_content(/Maybe in Russia|Probably in Russia/)
     end
