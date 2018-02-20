ea07792d17529bc2baf77ed8f5d71246d3c14913
Rustin Player <rjplayer@outlook.com>
2018-02-20 14:29:05 -0700

    Refactor to create active helper method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 2.times do |user|
-  User.create!(first_name: "Guest",
-               last_name: "#{user}", 
+  User.create!(first_name: "Edward",
+               last_name: "Snowden#{user}", 
                email: "test#{user}@test.test", 
                password: "aaaaaa", 
                password_confirmation: "aaaaaa"
@@ -11,7 +11,7 @@ end
 puts "2 users created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: User.last.id)
+  Post.create!(date: Date.today, rationale: "In Russia#{post}", user_id: User.last.id)
 end
 
 puts "100 posts have been created"
