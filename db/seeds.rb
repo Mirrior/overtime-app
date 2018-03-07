53878e10a0eaaf84b397b8bbd73cb0b22e0cf743
Rustin Player <rjplayer@outlook.com>
2018-03-07 10:04:39 -0700

    Brought in live data for admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ AdminUser.create!(first_name: "NotEdward",
 puts "admin user created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "In Russia#{post}", user_id: User.first.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "In Russia#{post} Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.", user_id: User.first.id, overtime_request: 2.5)
 end
 
 puts "100 posts have been created"
