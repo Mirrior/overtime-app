487c8e7cee36cea3f966ee65cf4383a52077d7e8
Rustin Player <rjplayer@outlook.com>
2018-03-07 12:12:27 -0700

    Styled the employee homepage pending audit approvals

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,13 @@
-2.times do |user|
-  User.create!(first_name: "Edward",
-               last_name: "Snowden#{user}", 
-               email: "test#{user}@test.test", 
-               password: "aaaaaa", 
-               password_confirmation: "aaaaaa",
-               phone: "3852437428"
-              )
+User.create!(first_name: "Edward",
+             last_name: "Snowden", 
+             email: "test@test.test", 
+             password: "aaaaaa", 
+             password_confirmation: "aaaaaa",
+             phone: "3852437428"
+            )
 
-end
 
-puts "2 users created"
+puts "regular user created"
 
 AdminUser.create!(first_name: "NotEdward",
                last_name: "NotSnowden", 
@@ -27,9 +25,8 @@ end
 
 puts "100 posts have been created"
 
+AuditLog.create!(user_id: User.first.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: User.first.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: User.first.id, status: 0, start_date: (Date.today - 20.days))
 
-100.times do |audit_log|
-  AuditLog.create!(user_id: User.first.id, status: 0, start_date: Date.today)
-end
-
-puts "100 audit logs have been created"
+puts "3 audit logs have been created"
