dbc3098da56507a56f700c13e85992d2d8ccaf9c
Rustin Player <rjplayer@outlook.com>
2018-03-08 13:40:43 -0700

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,14 +19,14 @@ AdminUser.create!(first_name: "NotEdward",
 
 puts "admin user created"
 
-100.times do |post|
-  Post.create!(date: Date.today, rationale: "In Russia#{post} Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.", user_id: User.first.id, overtime_request: 2.5)
-end
-
-puts "100 posts have been created"
-
 AuditLog.create!(user_id: User.first.id, status: 0, start_date: (Date.today - 6.days))
 AuditLog.create!(user_id: User.first.id, status: 0, start_date: (Date.today - 13.days))
 AuditLog.create!(user_id: User.first.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
+
+100.times do |post|
+  Post.create!(date: Date.today, rationale: "In Russia#{post} Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.", user_id: User.first.id, overtime_request: 2.5)
+end
+
+puts "100 posts have been created"
