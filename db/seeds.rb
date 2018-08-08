83b70b4f675068132c56ccf6b3cea01e76a8a371
Rustin Player <rjplayer@outlook.com>
2018-08-07 23:17:42 -0600

    Minor comment fix for seeds setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,4 +29,4 @@ puts "3 audit logs have been created"
   Post.create!(date: Date.today, rationale: "#{post} Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.", user_id: Employee.first.id, overtime_request: 2.5)
 end
 
-puts "100 posts have been created"
+puts "50 posts have been created"
